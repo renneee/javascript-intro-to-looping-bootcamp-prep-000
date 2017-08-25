2049f7c59a59297fc8f89b40c98bf8abb73d376a
renneee <renpyang@gmail.com>
2017-08-25 08:08:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,13 +20,12 @@ function whileLoop (number) {
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random () >= 0.5
-    
+
   }
   do{
     array = array.shift
   } while (array.length>0 && maybeTrue())
 
   return array
-  
-}
 
+}
