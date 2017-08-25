be7ae3d1c058504f530a4ecf782aa926a830d677
renneee <renpyang@gmail.com>
2017-08-25 07:17:38 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop (array) {
+  var array = [1, 2, 3, 4, 5]
+  for if (let i = 1) {
+    console.log(`I am 1 strange loop`);
+  }
+  else (let i = 1; i < 25; i++) {
+  console.log( "I am " + i + " strange loops" );
+}
+return array
+}
+
